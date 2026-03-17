e0e2d8a2dd47b727ee6402ebde8ae28bcd13c599
AaryanBairagi <aaryanbairagi11@gmail.com>
2026-03-17 20:18:59 +0530

    made severe changes in both frontend and backend including the components

--- client/lib/socket.js
+++ client/lib/socket.js
@@ -7,7 +7,8 @@ let socket
 export function getSocket() {
   if (!socket) {
     socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
-      transports: ["websocket"],
+      transports : ["websocket"],
+      secure : true
     })
   }
   return socket
